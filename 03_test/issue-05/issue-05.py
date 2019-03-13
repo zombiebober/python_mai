18ce351111c2b9325c98faab6fcab21ced5e1f17
Dima <zombibober@gmail.com>
2019-03-13 19:32:11 +0300

    task is closed

--- 03_test/issue-05/issue-05.py
+++ 03_test/issue-05/issue-05.py
@@ -9,10 +9,11 @@
     нет замечаний от flake8
 """
 from unittest.mock import patch
-import  pytest
+import pytest
 import urllib.request
 import json
 
+# $#python -m pytest --cov=scrapy  --cov  issue-05.py
 API_URL = 'http://worldclockapi.com/api/json/utc/now'
 
 YMD_SEP = '-'
@@ -26,7 +27,8 @@ DMY_YEAR_SLICE = slice(DMY_SEP_INDEX + 1, DMY_SEP_INDEX + 5)
 
 def what_is_year_now() -> int:
     """
-    Получает текущее время из API-worldclock и извлекает из поля 'currentDateTime' год
+    Получает текущее время из API-worldclock
+    и извлекает из поля 'currentDateTime' год
 
     Предположим, что currentDateTime может быть в двух форматах:
       * YYYY-MM-DD - 2019-03-01
@@ -53,20 +55,31 @@ def test_get_what_is_year_now():
         year = what_is_year_now()
     assert year == exp_year
 
-def test_wiyn_none():
-    exp_json = dict(currentDateTime='')
-    with pytest.raises(IndexError):
-        with patch.object(json, 'load', return_value=exp_json):
-            year = what_is_year_now()
 
-def test_get_what_is_year_now2():
+# def test_what_is_year_now_ie():
+#     exp_json = dict(currentDateTime='')
+#     with pytest.raises(IndexError):
+#         with patch.object(json, 'load', return_value=exp_json):
+#             year = what_is_year_now()
+
+
+def test_what_is_year_now_valuee_error():
     exp_json = dict(currentDateTime='2015235236')
     with pytest.raises(ValueError):
         with patch.object(json, 'load', return_value=exp_json):
-            year = what_is_year_now()
+            what_is_year_now()
 
-def test_get_what_is_year_now3():
-    exp_json = dict()
-    with pytest.raises(KeyError):
-        with patch.object(json, 'load', return_value=exp_json):
-            year = what_is_year_now()
+
+# def test_what_is_year_now_ke():
+#     exp_json = dict()
+#     with pytest.raises(KeyError):
+#         with patch.object(json, 'load', return_value=exp_json):
+#             year = what_is_year_now()
+
+
+def test_get_what_is_year_now_r():
+    exp_json = dict(currentDateTime='21.03.2017')
+    year = 2017
+    with patch.object(json, 'load', return_value=exp_json):
+        json_year = what_is_year_now()
+    assert year == json_year
